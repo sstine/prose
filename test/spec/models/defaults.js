e585cc1abc2cfdda1b76cf21c75f83247220da67
Derek Lieu <dl@dereklieu.com>
2016-05-15 15:23:19 -0400

    Set defaults on org, correct url on orgs

--- test/spec/models/defaults.js
+++ test/spec/models/defaults.js
@@ -1,12 +1,7 @@
 var _ = require('underscore');
 var User = require('../../../app/models/user');
 
-
 describe('models set defaults', function () {
-  it('sets defaults on User', function () {
-    var model = new User({});
-    expect(_.difference(['login', 'id', 'type', 'avatar_url'], _.keys(model.attributes)).length).not.ok;
-  });
   it('includes a login property on User', function () {
     var model = new User({});
     model.api = 'gitlab';
